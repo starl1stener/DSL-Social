56aaba832556c1adf0902aeedc0d588ad05aa7dd
starl1stener <reaeon@gmail.com>
2016-11-12 17:57:02 +0300

    UI created, assets added

---
+++ Nag Social/FancyField.swift
@@ -0,0 +1,21 @@
+//
+//  FancyField.swift
+//  Nag Social
+//
+//  Created by nag on 12/11/2016.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Nag Social/SignInVC.swift
+++ Nag Social/SignInVC.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class SignInVC: UIViewController {
-
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        
+    }
+    
+    
 }
 
