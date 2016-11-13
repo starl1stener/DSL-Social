ce0b041b416f1a75875f577bc5858772b96f6f8a
starl1stener <reaeon@gmail.com>
2016-11-13 17:18:10 +0300

    custom cell added

---
+++ Nag Social/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  Nag Social
+//
+//  Created by nag on 13/11/2016.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    @IBOutlet weak var likeImg: UIImageView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    
+
+}
